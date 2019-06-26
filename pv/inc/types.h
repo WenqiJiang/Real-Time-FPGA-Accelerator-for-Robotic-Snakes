4ebd65bbbda60fb5c59ae997b2e29ddd94869d34
WenqiJiang <wenqi.j@columbia.edu>
2019-06-26 09:59:43 -0400

    main.cc

--- pv/inc/types.h
+++ pv/inc/types.h
@@ -12,7 +12,11 @@
 #define FXD_I_LENGTH 7
 #define FDATA_T ap_fixed<FXD_W_LENGTH,FXD_I_LENGTH>
 #define TOFLOAT(x) x.to_float()
+#define MALLOC sds_malloc
+#define MFREE sds_free
 
 #else
 #define FDATA_T float
+#define MALLOC malloc
+#define MFREE free
 #endif
