294fe2deb52219a71e24972ea1bc18077be8110a
WenqiJiang <wenqi.j@columbia.edu>
2019-07-17 17:34:14 -0400

    add files

---
+++ programmer_view/floating_point/inc/configs.h
@@ -0,0 +1,60 @@
+#pragma once
+
+  // LSTM Layer 1 weights
+  char const* FORGET_GATE_KERNEL_LAST_STATE_1 =
+      "../../../model/forget_gate_kernel_last_state_1.txt";
+  char const* FORGET_GATE_KERNEL_INPUT_STATE_1 =
+      "../../../model/forget_gate_kernel_input_state_1.txt";
+  char const* FORGET_GATE_BIAS_1 =
+      "../../../model/forget_gate_bias_1.txt";
+  char const* INPUT_GATE_KERNEL_LAST_STATE_1 =
+      "../../../model/input_gate_kernel_last_state_1.txt";
+  char const* INPUT_GATE_KERNEL_INPUT_STATE_1 =
+      "../../../model/input_gate_kernel_input_state_1.txt";
+  char const* INPUT_GATE_BIAS_1 =
+      "../../../model/input_gate_bias_1.txt";
+  char const* TANH_GATE_KERNEL_LAST_STATE_1 =
+      "../../../model/tanh_gate_kernel_last_state_1.txt";
+  char const* TANH_GATE_KERNEL_INPUT_STATE_1 =
+      "../../../model/tanh_gate_kernel_input_state_1.txt";
+  char const* TANH_GATE_BIAS_1 =
+      "../../../model/tanh_gate_bias_1.txt";
+  char const* OUTPUT_GATE_KERNEL_LAST_STATE_1 =
+      "../../../model/output_gate_kernel_last_state_1.txt";
+  char const* OUTPUT_GATE_KERNEL_INPUT_STATE_1 =
+      "../../../model/output_gate_kernel_input_state_1.txt";
+  char const* OUTPUT_GATE_BIAS_1 =
+      "../../../model/output_gate_bias_1.txt";
+
+  // LSTM Layer 2 weights
+  char const* FORGET_GATE_KERNEL_LAST_STATE_2 =
+      "../../../model/forget_gate_kernel_last_state_2.txt";
+  char const* FORGET_GATE_KERNEL_INPUT_STATE_2 =
+      "../../../model/forget_gate_kernel_input_state_2.txt";
+  char const* FORGET_GATE_BIAS_2 =
+      "../../../model/forget_gate_bias_2.txt";
+  char const* INPUT_GATE_KERNEL_LAST_STATE_2 =
+      "../../../model/input_gate_kernel_last_state_2.txt";
+  char const* INPUT_GATE_KERNEL_INPUT_STATE_2 =
+      "../../../model/input_gate_kernel_input_state_2.txt";
+  char const* INPUT_GATE_BIAS_2 =
+      "../../../model/input_gate_bias_2.txt";
+  char const* TANH_GATE_KERNEL_LAST_STATE_2 =
+      "../../../model/tanh_gate_kernel_last_state_2.txt";
+  char const* TANH_GATE_KERNEL_INPUT_STATE_2 =
+      "../../../model/tanh_gate_kernel_input_state_2.txt";
+  char const* TANH_GATE_BIAS_2 =
+      "../../../model/tanh_gate_bias_2.txt";
+  char const* OUTPUT_GATE_KERNEL_LAST_STATE_2 =
+      "../../../model/output_gate_kernel_last_state_2.txt";
+  char const* OUTPUT_GATE_KERNEL_INPUT_STATE_2 =
+      "../../../model/output_gate_kernel_input_state_2.txt";
+  char const* OUTPUT_GATE_BIAS_2 =
+      "../../../model/output_gate_bias_2.txt";
+
+  // Layer 1, a super large array, contain the inputs of all steps
+  char const* LSTM_INPUT_STATE_1 = "../../../model/lstm_input_state_1.txt";
+
+  // fc weights
+  char const* FC_KERNEL = "../../../model/fc_kernel.txt";
+  char const* FC_BIAS = "../../../model/fc_bias.txt";
